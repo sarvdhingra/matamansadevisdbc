7520ad63d7347c110ed4bc70d0684b8d8deeb598
Rohit Dhingra <er.rohit.dhingra@gmail.com>
2014-11-15 19:28:25 +0530

    Member Login Page Changes
    Member Login Page Error Changes

---
+++ MataMansaDeviProject/src/com/matamansadevisdbc/LoginAction.java
@@ -0,0 +1,26 @@
+package com.matamansadevisdbc;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+public class LoginAction extends Action{
+	private final static String SUCCESS = "success";
+
+	private final static String FAILURE = "failure";
+
+	public ActionForward execute(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		LoginForm loginForm = (LoginForm) form;
+		if (loginForm.getUserName().equals(loginForm.getPassword())) {
+			return mapping.findForward(SUCCESS);
+		} else {
+			return mapping.findForward(FAILURE);
+		}
+	}
+}
